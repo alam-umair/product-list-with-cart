6862e78360270ebd9d8be2c20c1ed619b49f0da2
alam-umair <umair20nov1@gmail.com>
2025-08-12 17:56:48 +0530

    button functionality for increasing and reducing number of items have been added

--- assets/view/dessertPage.js
+++ assets/view/dessertPage.js
@@ -6,17 +6,39 @@ dessertList
     const list = document.querySelector(".list-container"); //this is the ul
     const template = document.querySelector(".list-item"); //this is the li template
     data.forEach((item) => {
-      console.log("item", item.name);
+      // console.log("item", item.name);
       const clone = template.cloneNode(true);
-      template.style.display = "flex"; // Ensure the template is displayed as flex
+      clone.style.display = "flex"; // Ensure the template is displayed as flex
 
       clone.querySelector("img.list-image").src = item.image.desktop;
       clone.querySelector(".item-category").textContent = item.category;
       clone.querySelector(".item-name").textContent = item.name;
       clone.querySelector(".item-cost").textContent = `$${item.price}`;
 
+      const addOneQuantity = clone.querySelector(".addOneQuantity");
+      const removeOneQuantity = clone.querySelector(".removeOneQuantity");
+
+      addOneQuantity.addEventListener("click", () => {
+        let itemQuantity = clone.querySelector(".item-quantity");
+        let quantity = Number(itemQuantity.textContent);
+        if (quantity < 20) {
+          quantity = quantity + 1;
+        }
+        itemQuantity.textContent = quantity;
+      });
+
+      removeOneQuantity.addEventListener("click", () => {
+        let itemQuantity = clone.querySelector(".item-quantity");
+        let quantity = Number(itemQuantity.textContent);
+        if (quantity > 0) {
+          quantity -= 1;
+        }
+        itemQuantity.textContent = quantity;
+      });
+
       list.appendChild(clone);
     });
+    template.remove();
   })
   .catch((err) => {
     console.log("unable to fetch data", +err);
